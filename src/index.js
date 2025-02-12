ff00a2d2e28bf47562b3eb447ffb0fe97b00a2de
Shane <6071159+smashedr@users.noreply.github.com>
2025-02-11 23:25:20 -0800

    Add to GitHub

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const core = require('@actions/core')
+// const github = require('@actions/github')
+
+;(async () => {
+    try {
+        // Debug
+        // console.log('github.context:', github.context)
+        // console.log('process.env:', process.env)
+
+        // Inputs
+        const token = core.getInput('token', { required: true })
+        core.info(`token: ${token}`)
+
+        // Action
+        const result = token
+        console.log('result:', result)
+
+        // Outputs
+        core.setOutput('results', result)
+
+        core.info(`✅ \u001b[32;1mFinished Success`)
+    } catch (e) {
+        core.debug(e)
+        core.info(e.message)
+        core.setFailed(e.message)
+    }
+})()
