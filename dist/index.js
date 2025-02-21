7204b9fce8ab48cfaa8e56bed53a638209e7dcab
Shane <6071159+smashedr@users.noreply.github.com>
2025-02-21 15:38:57 -0800

    Test

--- dist/index.js
+++ dist/index.js
@@ -27582,6 +27582,24 @@ const core = __nccwpck_require__(7484)
         // Outputs
         core.setOutput('results', result)
 
+        // Job Summary
+        core.info('📝 Writing Job Summary')
+        core.summary.addRaw('## Test Action\n')
+        core.summary.addRaw('I did it!\n')
+
+        // $GITHUB_SERVER_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID
+        core.summary.addRaw(
+            `\n\n<sup><sub><a href="${process.env.$GITHUB_SERVER_URL}/${process.env.$GITHUB_REPOSITORY}/actions/run/${process.env.$GITHUB_RUN_ID}">` +
+                `View Logs</a></sup></sub>\n\n`
+        )
+
+        const text = 'View Documentation, Report Issues or Request Features'
+        const link = 'https://github.com/smashedr/test-action'
+        core.summary.addRaw(
+            `\n[${text}](${link}?tab=readme-ov-file#readme)\n\n---`
+        )
+        await core.summary.write()
+
         core.info(`✅ \u001b[32;1mFinished Success`)
     } catch (e) {
         core.debug(e)
